33af7391094474a0941c67e3ef1fc64eeee9876a
WaelFa <wael.fadhel93@gmail.com>
2019-05-19 23:18:03 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navigation from './navbar.js'
+import Paragraphs from './paragraphs.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Paragraphs />
+    </div>
+  );
+}
+
+export default App;
